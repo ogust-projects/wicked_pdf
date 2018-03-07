55bbfc0714346652eacefd57ea73cc9723d1540d
seb-sykio <s.delcour@sykio.fr>
2018-03-07 21:11:43 +0100

    remove render

--- lib/wicked_pdf/pdf_helper.rb
+++ lib/wicked_pdf/pdf_helper.rb
@@ -1,37 +1,5 @@
 class WickedPdf
   module PdfHelper
-    def self.included(base)
-      # Protect from trying to augment modules that appear
-      # as the result of adding other gems.
-      return if base != ActionController::Base
-
-      base.class_eval do
-        alias_method_chain :render, :wicked_pdf
-        alias_method_chain :render_to_string, :wicked_pdf
-        after_filter :clean_temp_files
-      end
-    end
-
-    def self.prepended(base)
-      # Protect from trying to augment modules that appear
-      # as the result of adding other gems.
-      return if base != ActionController::Base
-
-      base.class_eval do
-        after_action :clean_temp_files
-
-        alias_method :render_without_wicked_pdf, :render
-        alias_method :render_to_string_without_wicked_pdf, :render_to_string
-
-        def render(options = nil, *args, &block)
-          render_with_wicked_pdf(options, *args, &block)
-        end
-
-        def render_to_string(options = nil, *args, &block)
-          render_to_string_with_wicked_pdf(options, *args, &block)
-        end
-      end
-    end
 
     def render_with_wicked_pdf(options = nil, *args, &block)
       if options.is_a?(Hash) && options.key?(:pdf)
